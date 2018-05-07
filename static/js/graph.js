e820c8ea9376fdbbd69c0e17ae4c501baab512ab
harryphelps <harry.phelps@octoenergy.com>
2018-05-07 14:49:57 +0100

    Add pie chart for commit

--- static/js/graph.js
+++ static/js/graph.js
@@ -7,23 +7,36 @@ function makeGraphs(error, sportsData) {
 var ndx = crossfilter(sportsData);
 
 number_of_sports(ndx);
+run_type_pie_chart(ndx);
 
 dc.renderAll();
 }
 
-function number_of_sports(ndx) {
-    var name_dim = ndx.dimension(dc.pluck('type'));
-    var total_activities_per_sport = name_dim.group();
+function run_type_pie_chart(ndx){
+    var type_dim = ndx.dimension(dc.pluck('commute'))
+    var total_distance_by_type = type_dim.group().reduceSum(dc.pluck('distance'));
 
-    dc.barChart("#numberOfSports")
-    .width(300)
-    .height(150)
-    .margins({top: 10, right: 50, bottom: 30, left: 50})
-    .dimension(name_dim)
-    .group(total_activities_per_sport)
-    .transitionDuration(500)
-    .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
-    .xAxisLabel("Sports")
-    .yAxis().ticks(4);
+        dc.pieChart("#commutePieChart")
+            .height(330)
+            .radius(90)
+            .transitionDuration(1500)
+            .dimension(type_dim)
+            .group(total_distance_by_type);
+}
+
+function number_of_sports(ndx) {
+    var type_dim = ndx.dimension(dc.pluck('type'));
+    var total_activities_per_sport = type_dim.group();
+
+        dc.barChart("#numberOfSports")
+        .width(300)
+        .height(150)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(type_dim)
+        .group(total_activities_per_sport)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Sports")
+        .yAxis().ticks(4);
 };
