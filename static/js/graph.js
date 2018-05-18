20254d8c49da0cda05e8406543e5d06776487b50
harryphelps <harry.phelps@octoenergy.com>
2018-05-18 10:39:31 +0100

    Pass in length variable from dc function into google map function

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,24 +1,26 @@
 queue()
 	.defer(d3.json, "data/sportsData.json")
 	.await(makeGraphs);
+
+
+//Variables and Constants
 	
 const earthCircumference = 40075000;
 const earthRadius = 6378000;
+var apiKey = "AIzaSyB9XZkjWuLN5EpJoMqk9e5SVkCQc4-mC3k"
+var homeLat = 51.4;
+var homeLong = -0.00098;
 
 function makeGraphs(error, sportsData) {
 	
-var ndx = crossfilter(sportsData);
+    var ndx = crossfilter(sportsData);
 
-run_type_pie_chart(ndx);
-number_of_sports_over_time(ndx, sportsData);
-average_cadence_against_average_speed(ndx);
-total_distance(ndx);
-percentage_round_world(ndx);
-// total_elevation(ndx);
+    run_type_pie_chart(ndx);
+    number_of_sports_over_time(ndx, sportsData);
+    average_cadence_against_average_speed(ndx);
+    total_distance(ndx);
 
-dc.renderAll();
-
-
+    dc.renderAll();
 }
 
 
@@ -36,9 +38,10 @@ function run_type_pie_chart(ndx){
 
 function number_of_sports_over_time(ndx, sportsData) {
     var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%SZ").parse;
+    
     sportsData.forEach(function(d){
         d.start_date = parseDate(d.start_date);
-    })
+    });
 
     var date_dim = ndx.dimension(dc.pluck('start_date'));
     var total_distance_per_month = date_dim.group().reduceSum(dc.pluck('distance'));
@@ -89,7 +92,7 @@ function print_filter(filter) {
     if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
     if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
     console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
-}
+};
 
 function total_distance(ndx) {
     var distance_dim = ndx.dimension(function(d){
@@ -103,30 +106,14 @@ function total_distance(ndx) {
     print_filter(totalDistance)
     console.log(totalDistance)
 
-//need to get it to return a 
-
     dc.numberDisplay("#distanceRan")
         .formatNumber(d3.format(".1f"))
         .valueAccessor(function(d){
+        //Put initMap function in here to access the length value
+            initMap(d.value)
             return d.value / 1000
         })
         .group(totalDistance);
-};
-
-
-function percentage_round_world(ndx) {
-    var distance_dim = ndx.dimension(function(d){
-        return d.type
-    });
-
-    var totalDistance = distance_dim.group().reduceSum(function(d){
-        return d.distance;
-    });
-  
-    // print_filter(totalDistance)
-    // console.log(totalDistance)
-
-//need to get it to return a 
 
     dc.numberDisplay("#percentageRoundWorld")
         .formatNumber(d3.format(".2%"))
@@ -136,25 +123,19 @@ function percentage_round_world(ndx) {
         .group(totalDistance);
 };
 
-var apiKey = "AIzaSyB9XZkjWuLN5EpJoMqk9e5SVkCQc4-mC3k"
+function initMap(length) {
 
-function initMap() {
-
-var homeLat = 51.4;
-var homeLong = -0.00098;
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 3,
       center: {lat: homeLat, lng: homeLong},
       mapTypeId: 'terrain'
     });
 
-
-totalDistance = 2609939;
-var finishLat = homeLat - (totalDistance / earthRadius) * (180/Math.PI);
-
-console.log(finishLat);
-
-
+//Need to set the total distance from the other function as a global variable to be able to pass in to this. 
+//Or just pass in the function?
+//Need the function to pass  
+    totalDistance = length;
+    var finishLat = homeLat - (totalDistance / earthRadius) * (180/Math.PI);
 
     var pathCoordinates = [
       {lat: homeLat, lng: homeLong},
@@ -171,28 +152,6 @@ console.log(finishLat);
     crowFliesPath.setMap(map);
     }
 
-// function total_elevation(ndx) {
-//     var type_dim = ndx.dimension(function(d){
-//         return d.type
-//     });
-
-//     var totalElevation = type_dim.group().reduceSum(function(d){
-//         return d.total_elevation_gain;
-//     });
-  
-//   //returning NaN
-
-//     print_filter(totalElevation)
-//     console.log(totalElevation)
-
-// //need to get it to return a 
-
-//     dc.numberDisplay("#elevationGained")
-//         .valueAccessor(function(d){
-//             return d.value 
-//         })
-//         .group(totalElevation);
-// };
 
    
 
